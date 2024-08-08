e941a8b7c1d0cbdc57f8c6294804c716e22eaf14
Lakshay Bajaj <lakshaybajaj@yahoo.com>
2024-08-08 13:32:32 +0530

    modified:   App.tsx 	modified:   babel.config.js 	modified:   package-lock.json 	modified:   package.json

--- App.tsx
+++ App.tsx
@@ -1,118 +1,115 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import { StyleSheet, Text, View, TextInput, Pressable, Modal, SafeAreaView } from 'react-native'
+import React, { useCallback, useEffect, useState } from 'react'
+// import { Slider } from '@miblanchard/react-native-slider';
+import Slider from '@react-native-community/slider';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+const App = () => {
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  const [length, setLength] = useState(10)
+  const [numberAllowed, setNumberAllowed] = useState(false)
+  const [charAllowed, setCharAllowed] = useState(false)
+  const [password, setPassword] = useState("")
+  const [numberColor, setNumberColor] = useState('#ffffff')
+  const [charColor, setCharColor] = useState('#ffffff')
+  const passwordGenerator = useCallback(() => {
+    let pass = ""
+    let str = "ABCDEFGHIJKLMNOPQRSTUUVWXYZabcdedfghijklmnopqrstuvwxyz"
+    if (numberAllowed) {
+      str += "1234567890"
+    }
+    if (charAllowed) {
+      str += "!@#$%^&*(){}:?></,.';;][=-~`"
+    }
+    for (let i = 0; i < length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1)
+      pass += str.charAt(char)
+    }
+    setPassword(pass)
+  }, [length, numberAllowed, charAllowed, setPassword])
+  useEffect(() => {
+    passwordGenerator()
+  }, [length, charAllowed, numberAllowed, passwordGenerator])
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+
+    <SafeAreaView style={styles.container}>
+      <SafeAreaView style={styles.subContainer}>
+        <Slider
+         thumbTintColor="#ffffff"
+         style={{
+          backgroundColor:"#1d222e",
+          width:'100%',
+          height:100,
+         }}
+         shouldRasterizeIOS={true}
+          minimumValue={6}
+          maximumValue={100}
+          value={length}
+          onValueChange={(value) => setLength(value)}
+          minimumTrackTintColor='#f29e74'
+          maximumTrackTintColor='#c4fb80'
+          
+        />
+
+        <Text style={styles.passwordTxt} selectable={true}>{password}</Text>
+
+        <View style={styles.numberContainer}>
+
+          <Text style={styles.numberTxt}>add number</Text>
+
+          <View>
+            <Pressable onPress={() => {
+              setNumberAllowed(!numberAllowed)
+
+              if (numberAllowed) {
+                setNumberColor('#b15359')
+              }
+              else {
+
+                setNumberColor('#c9e65b')
+
+              }
+            }}
+              style={[styles.numberBtn, { backgroundColor: numberColor }]}>
+
+              <Text>            </Text>
+
+            </Pressable>
+          </View>
+
         </View>
-      </ScrollView>
+        <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+          <Text style={{ color: "#d99fbc", fontSize: 30 }}>add Characters</Text>
+
+          <Pressable onPress={() => {
+            { setCharAllowed(!charAllowed) }
+
+            charAllowed ? setCharColor("#b15359") : setCharColor('#c9e65b')
+
+          }}
+            style={[styles.charBtn, { backgroundColor: charColor }]}>
+
+            <Text>            </Text>
+
+          </Pressable>
+
+        </View>
+      </SafeAreaView>
     </SafeAreaView>
-  );
+  )
 }
+
+export default App
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
+  container: { flex: 1, backgroundColor: '#1f2430' },
+  subContainer: { paddingHorizontal: 5 },
+  passwordTxt: { fontSize: 20, color: '#ffb252' },
+  numberContainer: { flexDirection: 'row', justifyContent: 'space-between' },
+  numberTxt: { color: '#d99fbc', fontSize: 30 },
+  numberBtn: { margin: 3, borderRadius: 50, width: 50, height: 50 },
+  charBtn: { margin: 3, borderRadius: 50, width: 50, height: 50 }
+})
